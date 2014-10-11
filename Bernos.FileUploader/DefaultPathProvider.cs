5dc5a1ccf374629ee57635fa881ac5da224da093
Brendan McMahon <bernos@gmail.com>
2014-10-11 16:05:17 +1100

    Added default path provider

---
+++ Bernos.FileUploader/DefaultPathProvider.cs
@@ -0,0 +1,17 @@
+﻿namespace Bernos.FileUploader
+{
+    public class DefaultPathProvider : IPathProvider
+    {
+        private readonly string _rootPath;
+
+        public DefaultPathProvider(string rootPath)
+        {
+            _rootPath = rootPath;
+        }
+
+        public string GetRootPath()
+        {
+            return _rootPath;
+        }
+    }
+}
