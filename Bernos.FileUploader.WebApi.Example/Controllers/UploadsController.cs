43a7e429a9384a8227be3dd0c634118da6c374a8
Brendan McMahon <bernos@gmail.com>
2014-10-11 16:13:41 +1100

    updated web api example. added extension methods to web api

--- Bernos.FileUploader.WebApi.Example/Controllers/UploadsController.cs
+++ Bernos.FileUploader.WebApi.Example/Controllers/UploadsController.cs
@@ -7,7 +7,9 @@ using System.Net;
 using System.Net.Http;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using System.Web;
 using System.Web.Http;
+using Bernos.FileUploader.StorageProviders.LocalFileSystem;
 
 namespace Bernos.FileUploader.WebApi.Example.Controllers
 {
@@ -15,45 +17,25 @@ namespace Bernos.FileUploader.WebApi.Example.Controllers
     {
         private readonly FileUploadService _fileUploadService;
 
+        public UploadsController()
+        {
+            _fileUploadService = new FileUploadService(new FileUploaderConfiguration
+            {
+                StorageProvider = new LocalFileSystemStorageProvider(new LocalFileSystemStorageProviderConfiguration(), new DefaultPathProvider(HttpContext.Current.Server.MapPath("~/")))
+            });
+        }
+
         public async Task<HttpResponseMessage> Post()
         {
-
-            var streamProvider = new MultipartMemoryStreamProvider();
-            await Request.Content.ReadAsMultipartAsync(streamProvider);
-
-            var results = new Collection<UploadedFile>();
-
-
-            foreach (var content in streamProvider.Contents)
+            if (!Request.Content.IsMimeMultipartContent())
             {
-                using (var stream = await content.ReadAsStreamAsync())
-                {
-                    var result = _fileUploadService.UploadFile(new FileUploadRequest
-                    {
-                        Filename = content.Headers.ContentDisposition.FileName.Trim('"'),
-                        Folder = "",
-                        InputStream = stream,
-                        ContentType = content.Headers.ContentType.MediaType
-                    });
-
-                    results.Add(result);
-                }
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
             }
 
-            var response = Request.CreateResponse<ICollection<UploadedFile>>(HttpStatusCode.Created, results);
+            var results = await Request.UploadFilesAsync(_fileUploadService);
 
-            return response;
-            /*
-             var result = uploadService.UploadFile(new FileUploadRequest
-                    {
-                        Filename = file.Name,
-                        Folder = "",
-                        InputStream = file.Value,
-                        ContentType = file.ContentType,
-                        Metadata = metadata
-                    });
-             
-             */
+            return Request.CreateResponse(HttpStatusCode.Created, results);
         }
     }
+
 }
---
+++ Bernos.FileUploader.WebApi/HttpRequestMessageExtensions.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Bernos.FileUploader.WebApi
+{
+    public static class HttpRequestMessageExtensions
+    {
+        public static async Task<IEnumerable<FileUploadResponse>> UploadFilesAsync(this HttpRequestMessage request, IFileUploadService fileUploadService)
+        {
+            return await UploadFilesAsync(request, fileUploadService, string.Empty);
+        }
+
+        public static async Task<IEnumerable<FileUploadResponse>> UploadFilesAsync(this HttpRequestMessage request,
+            IFileUploadService fileUploadService, string folder)
+        {
+            return await UploadFilesAsync(request, fileUploadService, (content, fileUploadRequest) =>
+            {
+                if (!string.IsNullOrEmpty(folder))
+                {
+                    fileUploadRequest.Folder = folder;
+                }
+            });
+        }
+
+        public static async Task<IEnumerable<FileUploadResponse>> UploadFilesAsync(this HttpRequestMessage request, IFileUploadService fileUploadService, Action<HttpContent, FileUploadRequest> fileUploadRequestBuilder)
+        {
+            var streamProvider = new MultipartMemoryStreamProvider();
+
+            await request.Content.ReadAsMultipartAsync(streamProvider);
+
+            var tasks = streamProvider.Contents.Select(content => Task.Run(async () =>
+            {
+                using (var stream = await content.ReadAsStreamAsync())
+                {
+                    var fileUploadRequest = CreateFileUploadRequest(content, stream);
+
+                    if (fileUploadRequestBuilder != null)
+                    {
+                        fileUploadRequestBuilder(content, fileUploadRequest);
+                    }
+
+                    return await fileUploadService.UploadFileAsync(fileUploadRequest);
+                }
+            })).ToList();
+
+            return await Task.WhenAll(tasks);
+        }
+
+        private static FileUploadRequest CreateFileUploadRequest(HttpContent content, Stream stream)
+        {
+            return new FileUploadRequest
+            {
+                Filename = content.Headers.ContentDisposition.FileName.Trim('"'),
+                Folder = "",
+                InputStream = stream,
+                ContentType = content.Headers.ContentType.MediaType,
+                Metadata = new Dictionary<string, string>()
+            };
+        }
+    }
+}
