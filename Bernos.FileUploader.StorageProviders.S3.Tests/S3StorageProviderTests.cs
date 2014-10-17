92243bc678a1b29333baeaec399382a06861c272
Brendan McMahon <bmcmahon@seek.com.au>
2014-10-17 16:40:04 +1100

    Added test for handling non-existant files

---
+++ Bernos.FileUploader.StorageProviders.S3.Tests/S3StorageProviderTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Bernos.FileUploader.StorageProviders.S3.Tests
+{
+    public class S3StorageProviderTests
+    {
+        [Fact]
+        public void Should_Return_Null_When_Loading_Non_Existant_File()
+        {
+            var config = new S3StorageProviderConfiguration("bernos-bucket", "us-east-1", "uploads");
+
+            var service = new S3StorageProvider(config);
+
+            Assert.Null(service.Load("asdf"));
+        }
+    }
+}
