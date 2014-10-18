698333a745ca6f8dd28481a831c2673cd8908ea5
Brendan McMahon <bernos@gmail.com>
2014-10-18 11:09:47 +1100

    Added support for loading LocalFileSystemStorageProviderConfiguration from app settings

--- Bernos.FileUploader.StorageProviders.LocalFileSystem/LocalFileSystemStorageProviderConfiguration.cs
+++ Bernos.FileUploader.StorageProviders.LocalFileSystem/LocalFileSystemStorageProviderConfiguration.cs
@@ -1,7 +1,26 @@
-﻿namespace Bernos.FileUploader.StorageProviders.LocalFileSystem
+﻿using System;
+using System.Configuration;
+
+namespace Bernos.FileUploader.StorageProviders.LocalFileSystem
 {
     public class LocalFileSystemStorageProviderConfiguration
     {
+        public static LocalFileSystemStorageProviderConfiguration FromAppSettings()
+        {
+            var configuration = new LocalFileSystemStorageProviderConfiguration();
+
+            if (!String.IsNullOrEmpty(ConfigurationManager.AppSettings["LocalFileSystemStorageProvider:UploadPath"]))
+            {
+                configuration.UploadPath = ConfigurationManager.AppSettings["LocalFileSystemStorageProvider:UploadPath"];
+            }
+
+            if (!String.IsNullOrEmpty(ConfigurationManager.AppSettings["LocalFileSystemStorageProvider:BaseUrl"]))
+            {
+                configuration.BaseUrl = ConfigurationManager.AppSettings["LocalFileSystemStorageProvider:BaseUrl"];
+            }
+
+            return configuration;
+        }
         public string UploadPath { get; set; }
         public string BaseUrl { get; set; }
         
