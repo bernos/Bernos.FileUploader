758872a8e6ae44df4e83e67dd03cb4f64e2450a5
Brendan McMahon <bernos@gmail.com>
2014-11-23 21:42:06 +1100

    updated build scripts

--- build.ps1
+++ build.ps1
@@ -6,9 +6,13 @@ Param(
     [string]$version
 )
 
+$package_dir = ".\Dist"
+
 $psake_location = ".\packages\psake.4.4.1\tools\psake.psm1"
 
 $xunit_location = ".\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe"
+
+$test_result_dir = ".\TestResults"
 
 $configuration = "Release"
 
@@ -21,13 +25,15 @@ $projects = @(
 	"Bernos.FileUploader.StorageProviders.S3",
 	"Bernos.FileUploader.WebApi")
 
-$properties = @{
+$psake_properties = @{
     "configuration"		= $configuration;
     "version"			= $version;
     "projects"			= $projects;
 	"xunit_location"	= $xunit_location;
+	"test_result_dir"	= $test_result_dir;
+	"package_dir"		= $package_dir;
 }
 
 import-module $psake_location
 
-invoke-psake $build_file $task_list -Properties $properties
+invoke-psake $build_file $task_list -Properties $psake_properties
--- default.ps1
+++ default.ps1
@@ -3,13 +3,15 @@ Properties {
     $projects = $null
     $configuration = "Release"
 	$xunit_location = $null
+	$test_result_dir = ".\TestResults"
+	$package_dir = ".\Dist"
 }
 
-Task Default -Depends Test
+Task Default -Depends UnitTests
 
 Task Publish -Depends Package {
     foreach($project in $projects) {
-        Get-ChildItem | Where-Object -FilterScript {
+        Get-ChildItem -Path $package_dir | Where-Object -FilterScript {
             ($_.Name.Contains("$project.$version")) -and !($_.Name.Contains(".symbols")) -and ($_.Extension -eq '.nupkg')    
         } | ForEach-Object {
             exec { nuget push $_.FullName }
@@ -17,27 +19,39 @@ Task Publish -Depends Package {
     }
 }
 
-Task Package -Depends Set-Versions,Build {
+Task Package -Depends Set-Versions,UnitTests {
+
+	New-Item -Force -ItemType Directory -Path $package_dir
+
     foreach($project in $projects) {
-        Get-ChildItem -Path "$project\*.csproj" | ForEach-Object {            
-            exec { nuget pack -sym $_.FullName -Prop Configuration=$configuration }
+        Get-ChildItem -Path "$project\*.csproj" | ForEach-Object {        			    
+            exec { nuget pack -sym $_.FullName -Prop Configuration=$configuration -OutputDirectory $package_dir }
         }        
     }
 }
 
 Task Build -Depends Clean {
-    Exec { msbuild "$Solution" /t:Build /p:Configuration=$configuration /p:VisualStudioVersion=12.0 } 
+    Exec { msbuild "$Solution" /t:Build /p:Configuration=$configuration /p:VisualStudioVersion=12.0 /v:normal /nologo } 
 }
 
 Task Clean {
-    Exec { msbuild "$Solution" /t:Clean /p:Configuration=$configuration /p:VisualStudioVersion=12.0 } 
+    Exec { msbuild "$Solution" /t:Clean /p:Configuration=$configuration /p:VisualStudioVersion=12.0 /v:normal /nologo } 
 }
 
-Task Test -Depends Build {	
+Task UnitTests -Depends Build {	
+
+	New-Item -Force -ItemType Directory -Path $test_result_dir
+	Remove-Item $test_result_dir\* -Recurse
+
 	Get-ChildItem -Recurse -Include *Tests.csproj | ForEach-Object {
+		"Running unit tests from $_.BaseName" | Write-Host -ForegroundColor Yellow
+
 		$test_assembly = $_.DirectoryName + "\bin\" + $configuration + "\" + $_.BaseName + ".dll"
-	
-		exec { .$xunit_location "$test_assembly" /nunit test-results.xml }
+		$xml_file = $test_result_dir + "\" + $_.BaseName + ".xml"
+
+		exec { .$xunit_location "$test_assembly" /nunit $xml_file } "One or more unit tests failed!"
+
+		"" | Write-Host
 	}
 }
 
